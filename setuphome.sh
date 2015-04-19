d80bc0c266da5cbfaed7d88ed66570cdc7a4d0be
Kristof Katus <kristof.katus@gmail.com>
2015-04-19 10:54:28 +0200

    rm mc solarized.ini, wget original instead

--- setuphome.sh
+++ setuphome.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 
-# Install homeshick, my dotfiles, and vundle
+# Install homeshick, my dotfiles, vundle, and mc skin
 # git clone git://github.com/katusk/dotfiles.git $HOME/.homesick/repos/dotfiles
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+wget -P $HOME/.mc/colors https://raw.github.com/iwfmp/mc-solarized-skin/master/solarized.ini
 
 # Reminders
 set +x; echo "Do not forget the following:"
