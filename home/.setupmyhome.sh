3fa2f61ee3a450202d83f79f2fab78102f54cb94
Kristof Katus <kristof.katus@gmail.com>
2015-06-21 08:15:06 +0200

    vim: vundle to vim-plug migration

--- home/.setupmyhome.sh
+++ home/.setupmyhome.sh
@@ -6,18 +6,18 @@ function git_pull_or_clone {
     if(! (cd $2 && git pull)); then git clone $1 $2; fi
 }
 
-# Install homeshick, my dotfiles, vundle, etc.
+# Install homeshick, my dotfiles, vim plugin manager, etc.
 git_pull_or_clone git://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
 git_pull_or_clone git://github.com/katusk/dotfiles.git ~/.homesick/repos/dotfiles
-git_pull_or_clone git://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git_pull_or_clone git://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+wget -nc -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget -nc -P ~/.mc/colors https://raw.githubusercontent.com/iwfmp/mc-solarized-skin/master/solarized.ini
 wget -nc -P ~ https://raw.githubusercontent.com/willnorris/dotfiles/master/lynx/.lynx.lss
 mkdir -p ~/.vim/undodir
 
 # Set up environment
 ~/.homesick/repos/homeshick/bin/homeshick link
-vim +PluginInstall +qall
+vim +PlugUpgrade +PlugUpdate +qall
 tmux new -d
 tmux run "~/.tmux/plugins/tpm/scripts/install_plugins.sh"
 tmux run "~/.tmux/plugins/tpm/scripts/update_plugin.sh all"
