1161a30e464ecf45eacca30fd910ac19d00318bd
Kristof Katus <kristof.katus@gmail.com>
2016-06-18 20:26:35 +0200

    add wget_to_as

--- home/.setupmyhome.sh
+++ home/.setupmyhome.sh
@@ -6,18 +6,20 @@ function git_pull_or_clone {
     if(! (cd $2 && git pull)); then git clone $1 $2; fi
 }
 
+# Fetch $1 to directory $2 saved as $3
+function wget_to_as {
+    mkdir -p $2 && [ -e $2/$3 ] || wget -nc -O $2/$3 $1
+}
+
 # Install homeshick, my dotfiles, vim plugin manager, etc.
 git_pull_or_clone git://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
 git_pull_or_clone git://github.com/katusk/dotfiles.git ~/.homesick/repos/dotfiles
 git_pull_or_clone git://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 wget -nc -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget -nc -P ~/.config/mc/colors https://raw.githubusercontent.com/iwfmp/mc-solarized-skin/master/solarized.ini
-[ -e ~/.lynx.lss  ] ||
-    wget -nc -O ~/.lynx.lss https://raw.githubusercontent.com/willnorris/dotfiles/master/lynx.lss
-mkdir -p ~/.mintty/themes && [ -e ~/.mintty/themes/solarized-dark.minttyrc  ] ||
-    wget -nc -O ~/.mintty/themes/solarized-dark.minttyrc https://raw.githubusercontent.com/mavnn/mintty-colors-solarized/master/.minttyrc.dark
-[ -e ~/.dircolors  ] ||
-    wget -nc -O ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+wget_to_as https://raw.githubusercontent.com/willnorris/dotfiles/master/lynx.lss ~ .lynx.lss
+wget_to_as https://raw.githubusercontent.com/mavnn/mintty-colors-solarized/master/.minttyrc.dark ~/.mintty/themes solarized-dark.minttyrc
+wget_to_as https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark ~ .dircolors
 mkdir -p ~/.vim/undodir
 
 # Set up environment
