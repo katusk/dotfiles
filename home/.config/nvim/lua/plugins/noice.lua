b2723e3d15986eb131f8c2cd8b0e56bdf9bc66d7
Kristof Katus <kristof.katus@gmail.com>
2025-11-23 17:06:59 +0100

    Set conceal to false for all cmdline patterns of noice.nvim
    As seen here:
    https://github.com/folke/noice.nvim?tab=readme-ov-file#%EF%B8%8F-configuration

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -5,6 +5,15 @@ return {
     opts = {
       cmdline = {
         view = "cmdline", -- move command line to bottom
+        format = {
+          cmdline = { conceal = false },
+          search_down = { conceal = false },
+          search_up = { conceal = false },
+          filter = { conceal = false },
+          lua = { conceal = false },
+          help = { conceal = false },
+          input = { conceal = false },
+        },
       },
       presets = { command_palette = false }, -- do not pop-up command line tab completions at the top
     },
