034d727799e252c4716d95c69f477e418a99ea6b
Kristof Katus <kristof.katus@gmail.com>
2025-11-23 16:44:50 +0100

    Reenable noice.nvim with cmdline at bottom
    As seen here:
    https://github.com/LazyVim/LazyVim/discussions/1926

---
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/noice.nvim",
+    -- enabled = false,
+    opts = {
+      cmdline = {
+        view = "cmdline", -- move command line to bottom
+      },
+      presets = { command_palette = false }, -- do not pop-up command line tab completions at the top
+    },
+  },
+}
