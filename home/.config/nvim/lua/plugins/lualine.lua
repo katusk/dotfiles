3c6d4c4e2e680067587ca1a935239bc65428217e
Kristof Katus <kristof.katus@gmail.com>
2025-12-01 21:18:24 +0100

    Show file format in status bar of LazyVim

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, "encoding")
+      table.insert(opts.sections.lualine_x, "fileformat")
     end,
   },
 }
