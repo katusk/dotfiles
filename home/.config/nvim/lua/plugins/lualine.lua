6a896805609a7d610c9f70e05855e874686f9690
Kristof Katus <kristof.katus@gmail.com>
2025-12-02 19:19:37 +0100

    No visible lualine separators, show BOM

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,13 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "encoding")
+      opts.options.section_separators = " "
+      opts.options.component_separators = ""
+      table.insert(opts.sections.lualine_x, {
+        "encoding",
+        -- Show '[BOM]' when the file has a byte-order mark
+        show_bomb = true,
+      })
       table.insert(opts.sections.lualine_x, "fileformat")
     end,
   },
