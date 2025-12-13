50121c632a9b8abeda84a8024c85090fbb6c7e1a
Kristof Katus <kristof.katus@gmail.com>
2025-12-13 17:51:32 +0100

    add jk and CR keybinds to nvim

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<Esc>", { silent = true }) -- Exiting Insert mode: <M-j> jj jk kj fd ;; ,,
+vim.keymap.set("n", "<CR>", ":", { silent = true }) -- A bit more convenient access to Ex commands
